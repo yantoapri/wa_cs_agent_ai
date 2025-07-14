f1d2db3e1dd6766636111f1888eef1dcd0304f3b
yantoapri <yapri177@gmail.com>
2025-07-14 16:55:28 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   modules: ["@nuxtjs/supabase"],
-
   // Konfigurasi untuk Cloudflare Pages
   nitro: {
     preset: "cloudflare-pages",
